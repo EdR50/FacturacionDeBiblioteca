3e23eb4f0f65647b78d5dcdad90d4c5414d557ff
EdR50 <narutogr7@gmail.com>
2021-03-05 16:58:35 -0600

    Se ha creado la clase conexion y la clases Query que contiene todos los metodos para realizar consultas a la BD

---
+++ DataManager/CLS/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace DataManager.CLS
+{
+    public class Conexion
+    {
+        // Variable global para realizar la conexion a la bd
+        private string _mySqlString = "Server=localhost;Database=biblioteca;Uid=Desarrollo2;Pwd=admin;";
+        protected MySqlConnection _connection;
+
+        public Conexion()
+        {
+            _connection = new MySqlConnection(_mySqlString);
+        }
+
+        protected Boolean Connected()
+        {
+            var connected = false;
+
+            try
+            {
+                _connection.Open();
+                connected = true;
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e);
+                connected = false;
+            }
+
+            return connected;
+        }
+
+
+        protected void Disconnected()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
---
+++ DataManager/CLS/Query.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DataManager.CLS
+{
+    public class Query : Conexion
+    {
+        public Query()
+        {
+        }
+
+        private int ExecuteQuery(string queryTxt)
+        {
+            var rowsAffected = 0;
+
+            if (base.Connected())
+            {
+                MySqlCommand command = new MySqlCommand(queryTxt, _connection);
+                rowsAffected = command.ExecuteNonQuery();
+                base.Disconnected();
+            }
+
+            return rowsAffected;
+        }
+
+        private DataTable ExecuteSelectQuery(string selectQuery)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+
+            try
+            {
+                if (Connected())
+                {
+                    MySqlCommand command = new MySqlCommand(selectQuery, base._connection);
+                    command.CommandType = CommandType.Text;
+                    adapter.SelectCommand = command;
+                    adapter.Fill(table);
+                    Disconnected();
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return table;
+        }
+
+        public int Insert(string queryTxt)
+        {
+            return ExecuteQuery(queryTxt);
+        }
+
+        public int Update(string queryTxt)
+        {
+            return ExecuteQuery(queryTxt);
+        }
+
+        public int Delete(string queryTxt)
+        {
+            return ExecuteQuery(queryTxt);
+        }
+        
+        
+    }
+}
