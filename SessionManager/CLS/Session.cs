b69bf58a0206a04729658fae45aa5c38b7397cd2
EdR50 <narutogr7@gmail.com>
2021-03-06 00:38:30 -0600

    Singleton Pattern aplicado al proyecto

---
+++ SessionManager/CLS/Session.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SessionManager.CLS
+{
+    // Esta clase es el patron de diseño singleton
+
+    public sealed class Session
+    {
+        private static Session _instance;
+        private static readonly object Lock = new object();
+
+        // El constructor es privado por lo cual la clase no puede ser instanciada en otras clases
+        private Session()
+        {
+        }
+
+        public static Session Instance
+        {
+            get
+            {
+                // Check if instance needs to be created to avoid unnecessary lock
+                // everytime you request an instance of the service
+                if (_instance is null)
+                {
+                    // Lock thread so only one thread can create the first instance
+                    lock (Lock)
+                    {
+                        // Check if instance needs to be created
+                        // This is to avoid initial initialization by two threads.
+                        if (_instance is null)
+                        {
+                            _instance = new Session();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
